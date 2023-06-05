abe7eb263f878bc9e502935a4492fe7895d875a0
Dhayaa Anbajagane <32583510+DhayaaAnbajagane@users.noreply.github.com>
2023-06-05 09:43:07 -0500

    Reverting power.py to previous version

--- jax_cosmo/power.py
+++ jax_cosmo/power.py
@@ -17,14 +17,10 @@ def primordial_matter_power(cosmo, k):
     Pk = k^n
     """
     
-    try:
-        lin_oscillations = cosmo.A_lin*np.sin(cosmo.w_lin*k + cosmo.phi_lin)
-        log_oscillations = cosmo.A_log*np.sin(cosmo.w_log*np.log(k/cosmo.kstar) + cosmo.phi_log)
-        return k**cosmo.n_s * (1 + lin_oscillations +  log_oscillations)
+    lin_oscillations = cosmo.A_lin*np.sin(cosmo.w_lin*k + cosmo.phi_lin)
+    log_oscillations = cosmo.A_log*np.sin(cosmo.w_log*np.log(k/cosmo.kstar) + cosmo.phi_log)
+    return k**cosmo.n_s * (1 + lin_oscillations +  log_oscillations)
     
-    except:
-        return k**cosmo.n_s
-
 
 def linear_matter_power(cosmo, k, a=1.0, transfer_fn=tklib.Eisenstein_Hu, **kwargs):
     r"""Computes the linear matter power spectrum.
